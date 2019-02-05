20aa299248c1148750523918ffb85a5a0122caee
Murilo Sandiego <murilosandiego1@gmail.com>
2019-02-04 22:12:19 -0200

    Bump 1.0.1

--- examples/albums.js
+++ examples/albums.js
@@ -1,8 +1,8 @@
-import SpotifyWrapper from '../src/index';
+import Spotify from '../src/index';
 // eslint-disable-next-line import/no-extraneous-dependencies
 global.fetch = require('node-fetch');
 
-const spotify = new SpotifyWrapper({
+const spotify = new Spotify({
   token: 'BQBARlSy2Y5_8o6l28b_j_633L7ZK10_LN4nrrVXrrnyYl0OfvoQk5oGtp_MY1SBvc1qkQAvuNQJ2AUhJBEpVDa7OesaHR-X7ZvIBNP4pCuJot7CpKsSGvmgVmjGMokxlkTRqDTp8ngCX9SweNdzaK_FI6kqykEnVQ',
 });
 
